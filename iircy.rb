ab6d020708505be4db0431fe623e70c72973580e
Martin Stannard <mstannard@gmail.com>
2009-05-08 14:47:33 +1000

    moved text

--- iircy.rb
+++ iircy.rb
@@ -3,9 +3,9 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
-  @today = Date.today
-  @camp_start = Date.parse('20090515')
-  @days_until_camp = @camp_start.yday - @today.yday
+  @now = Time.now.utc
+  @camp_start = Time.utc(2009, 5, 15, 15, 0, 0)
+  @days_until_camp = ((@camp_start - @now) / (3600 * 24)).to_i
   haml :index
 end
 
