01c51aedc042e262be01d1602f108a0555d3b06b
Martin Stannard <mstannard@gmail.com>
2009-05-10 12:31:06 +1000

    cleaned up view, added helper, moved view into main file

--- iircy.rb
+++ iircy.rb
@@ -2,10 +2,46 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
+helpers do
+
+  def days_until_camp
+    now = Time.now.utc
+    camp_start = Time.utc(2009, 5, 15, 15, 0, 0)
+    ((camp_start - now) / (3600 * 24)).to_i
+  end
+  
+end
+
 get '/' do
-  @now = Time.now.utc
-  @camp_start = Time.utc(2009, 5, 15, 15, 0, 0)
-  @days_until_camp = ((@camp_start - @now) / (3600 * 24)).to_i
+  @days = days_until_camp
+  case 
+  when @days > 0
+    @big = 'NO'
+    @small = "only #{@days} sleeps to go..."
+  when @days == 0
+    @big = 'YES'
+    @small = 'Railscamp starts today!'
+  when @days > -4
+    @big = 'YES'
+    @small = "Railscamp is rockin'!"
+  else
+    @big = 'NO'
+    @small = 'Railscamp is over :('
+  end
   haml :index
 end
 
+get '/format=xml' do
+  @days = days_until_camp
+  builder do |xml|
+    xml.isitrailscampyet ((@days > 0 || @days < -4) ? "no" : "yes")
+  end
+end
+
+__END__
+  
+@@ index
+%div{:style => "text-align: center; font-weight: bold; font-family: arial;text-align: center; padding-top: 200px;"}
+  %div{:style => "font-size: 120pt;"}
+    = @big
+  %h4= @small
