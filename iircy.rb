5232d3989bf53ef76f7c1dee640e95840491749c
Martin Stannard <mstannard@gmail.com>
2009-06-30 11:04:01 +1000

    updated for next camp

--- iircy.rb
+++ iircy.rb
@@ -6,7 +6,7 @@ helpers do
 
   def days_until_camp
     now = Time.now.utc
-    camp_start = Time.utc(2009, 5, 15, 15, 0, 0)
+    camp_start = Time.utc(2009, 11, 19, 17, 0, 0)
     ((camp_start - now) / (3600 * 24)).to_i
   end
   
@@ -28,8 +28,6 @@ get '/' do
     @big = 'NO'
     @small = 'Railscamp is over :('
   end
-  @big = 'NO'
-  @small = 'Railscamp is over :(<br>See you next time'
   haml :index, :options => {:format => :html5,
                             :attr_wrapper => '"'}
 end
@@ -53,7 +51,7 @@ __END__
 %html{:lang => "en-au"}
   %head
     %meta{:charset =>"UTF-8"}
-    %title Is it Railscamp Yet Again?
+    %title Is it Railscamp Yet?
     %style{:type => "text/css", :media => "screen"}
       div {font: 700 1em helvetica,"helvetica neue", arial, sans-serif; margin: 0; padding: 220px 0 0 0; text-align: center;}
       h1 {font-size: 160px; padding: 0;margin: 0;}
